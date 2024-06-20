7eab744040c97e122f93ec4e7b5ead9cba6cf10e
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-06-20 16:57:03 -0300

    Adding filter list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth)
+  }, [list, currentMonth]);
   return (
     <C.Container>
       <C.Header>

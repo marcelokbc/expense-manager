7b24011d1a4603d0a2ea7ec193d605fd1b384c6a
Marcelo Silva <marcelo.cansilva@gmail.com>
2024-06-21 14:41:52 -0300

    Adding category and value

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -2,8 +2,7 @@ import { Item } from '../types/Item';
 
 export const getCurrentMonth = () => {
     let now = new Date();
-    let month = (now.getMonth() + 1).toString().padStart(2, '0');
-    return `${now.getFullYear()}-${month}`;
+    return `${now.getFullYear()}-${now.getMonth()+1}`;
 }
 
 export const filterListByMonth = (list: Item[], date: string): Item[] => {
@@ -21,3 +20,13 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
 
     return newList;
 }
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+}
+
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`;

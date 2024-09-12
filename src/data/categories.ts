3b135460eb08378df210be8b2bc2dbbd3122a0d5
Marcelo Silva marcelo.cansilva@gmail.com <marcelo.cansilva@gmail.com>
2024-09-11 19:04:27 -0700

    Adding categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,8 +2,24 @@ import { Category } from '../types/Category';
 
 export const categories: Category = {
     food: { title: 'Alimentação', color: 'blue', expense: true},
-    rent: { title: 'Aluguel', color: 'brown', expense: true},
+    uber: { title: 'Uber', color: 'red', expense: true},
     salary: { title: 'Salário', color: 'green', expense: false},
     water: { title: 'Sabesp', color: 'gray', expense: true},
-    electricity: { title: 'Eletropaulo', color: 'yellow', expense: true},
+    electricity: { title: 'Eletropaulo', color: 'yellow', expense: true },
+    gas: { title: 'Gás', color: 'orange', expense: true },
+    pets: { title: 'Pets', color: 'purple', expense: true },
+    pharmacy: { title: 'Farmácia', color: 'white', expense: true },
+    car: { title: 'Carro', color: 'pink', expense: true },
+    school: { title: 'Escola', color: 'brown', expense: true },
+    internet: { title: 'Claro', color: 'black', expense: true },
+    card: { title: 'Cartão Nubank Ju', color: 'white', expense: true },
+    card1: { title: 'Cartão Carrefour', color: 'white', expense: true },
+    card2: { title: 'Cartão Nubank Kbça', color: 'white', expense: true },
+    card3: { title: 'Cartão Assaí', color: 'white', expense: true },
+    healthplan: { title: 'Plano de Saúde', color: 'white', expense: true },
+    supplies: { title: 'Insumos Ju', color: 'white', expense: true },
+    laboratory: { title: 'Laboratório', color: 'white', expense: true },
+    vaccines: { title: 'Vacinas', color: 'white', expense: true },
+    transport: { title: 'Transporte', color: 'white', expense: true },
+    
 }
